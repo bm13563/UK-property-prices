763f09b70d557d5cb6ef7fc3a540181f956f168d
ben <bm13563@my.bristol.ac.uk>
2021-01-31 13:47:54 +0000

    Considering another refactor, need to add further unit tests and endpoint testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import { parseAndReturnData } from "../controllers/api.pricePaid";
 import { Router } from "./router";
 
-
 export const register = ( app: express.Application ) => {
     const apiRouter = new Router();
     apiRouter.addBase( app, "api" );
