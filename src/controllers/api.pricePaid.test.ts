97a62ac35072bea444278a83bf2ebbd5d6efb402
ben <bm13563@my.bristol.ac.uk>
2021-02-03 18:07:29 +0000

    Refactored tests to be less verbose

--- src/controllers/api.pricePaid.test.ts
+++ src/controllers/api.pricePaid.test.ts
@@ -1,52 +1,54 @@
 import { _generateArgumentMap, _generateDefaultArguments, _formatRequestUrl } from "./api.pricePaid";
 
+const argumentMap = {
+    buildingName: "paon=",
+    street: "street=",
+    town: "town=",
+    postcode: "postcode=",
+    detached: "ptype%5B%5D=lrcommon%3A",
+    semiDetached: "ptype%5B%5D=lrcommon%3A",
+    terraced: "ptype%5B%5D=lrcommon%3A",
+    flat: "ptype%5B%5D=lrcommon%3A",
+    other: "ptype%5B%5D=lrcommon%3A",
+    newBuild: "nb%5B%5D=",
+    notNewBuild: "nb%5B%5D=",
+    freehold: "et%5B%5D=lrcommon%3A",
+    leasehold: "et%5B%5D=lrcommon%3A",
+    minPrice: "min_price=",
+    maxPrice: "max_price=",
+    dateFrom: "min_date=",
+    dateTo: "max_date=",
+    perPage: "limit=",
+}
+
+const defaultArguments = {
+    buildingName: "",
+    street: "",
+    town: "",
+    postcode: "",
+    detached: "detached",
+    semiDetached: "semi-detached",
+    terraced: "terraced",
+    flat: "flat-maisonette",
+    other: "otherPropertyType",
+    newBuild: "true",
+    notNewBuild: "false",
+    freehold: "freehold",
+    leasehold: "leasehold",
+    minPrice: "",
+    maxPrice: "",
+    dateFrom: "",
+    dateTo: "",
+    perPage: "100",
+}
+
 describe('_generateArgumentMap api.pricePaid', () => {
-    const argumentMap = {
-        buildingName: "paon=",
-        street: "street=",
-        town: "town=",
-        postcode: "postcode=",
-        detached: "ptype%5B%5D=lrcommon%3A",
-        semiDetached: "ptype%5B%5D=lrcommon%3A",
-        terraced: "ptype%5B%5D=lrcommon%3A",
-        flat: "ptype%5B%5D=lrcommon%3A",
-        other: "ptype%5B%5D=lrcommon%3A",
-        newBuild: "nb%5B%5D=",
-        notNewBuild: "nb%5B%5D=",
-        freehold: "et%5B%5D=lrcommon%3A",
-        leasehold: "et%5B%5D=lrcommon%3A",
-        minPrice: "min_price=",
-        maxPrice: "max_price=",
-        dateFrom: "min_date=",
-        dateTo: "max_date=",
-        perPage: "limit=",
-    }
     it('Argument map is correct', () => {
         expect( _generateArgumentMap() ).toEqual( argumentMap );
     });
 });
 
 describe('_generateDefaultArguments api.pricePaid', () => {
-    const defaultArguments = {
-        buildingName: "",
-        street: "",
-        town: "",
-        postcode: "",
-        detached: "detached",
-        semiDetached: "semi-detached",
-        terraced: "terraced",
-        flat: "flat-maisonette",
-        other: "otherPropertyType",
-        newBuild: "true",
-        notNewBuild: "false",
-        freehold: "freehold",
-        leasehold: "leasehold",
-        minPrice: "",
-        maxPrice: "",
-        dateFrom: "",
-        dateTo: "",
-        perPage: "100",
-    }
     it('Argument map is correct', () => {
         expect( _generateDefaultArguments() ).toEqual( defaultArguments );
     });
@@ -73,46 +75,6 @@ describe('_formatRequestUrl api.pricePaid', () => {
         dateTo: "undefined",
         perPage: "undefined",
     }
-    const argumentMap = {
-        buildingName: "paon=",
-        street: "street=",
-        town: "town=",
-        postcode: "postcode=",
-        detached: "ptype%5B%5D=lrcommon%3A",
-        semiDetached: "ptype%5B%5D=lrcommon%3A",
-        terraced: "ptype%5B%5D=lrcommon%3A",
-        flat: "ptype%5B%5D=lrcommon%3A",
-        other: "ptype%5B%5D=lrcommon%3A",
-        newBuild: "nb%5B%5D=",
-        notNewBuild: "nb%5B%5D=",
-        freehold: "et%5B%5D=lrcommon%3A",
-        leasehold: "et%5B%5D=lrcommon%3A",
-        minPrice: "min_price=",
-        maxPrice: "max_price=",
-        dateFrom: "min_date=",
-        dateTo: "max_date=",
-        perPage: "limit=",
-    }
-    const defaultArguments = {
-        buildingName: "",
-        street: "",
-        town: "",
-        postcode: "",
-        detached: "detached",
-        semiDetached: "semi-detached",
-        terraced: "terraced",
-        flat: "flat-maisonette",
-        other: "otherPropertyType",
-        newBuild: "true",
-        notNewBuild: "false",
-        freehold: "freehold",
-        leasehold: "leasehold",
-        minPrice: "",
-        maxPrice: "",
-        dateFrom: "",
-        dateTo: "",
-        perPage: "100",
-    }
     it('Argument map is correct', () => {
         expect( _formatRequestUrl( inputArguments, argumentMap, defaultArguments ) ).toEqual( "https://landregistry.data.gov.uk/app/ppd/ppd_data.csv?header=true&ptype%5B%5D=lrcommon%3Adetached&ptype%5B%5D=lrcommon%3Asemi-detached&ptype%5B%5D=lrcommon%3Aterraced&ptype%5B%5D=lrcommon%3Aflat-maisonette&ptype%5B%5D=lrcommon%3AotherPropertyType&nb%5B%5D=true&nb%5B%5D=false&et%5B%5D=lrcommon%3Afreehold&et%5B%5D=lrcommon%3Aleasehold&limit=100" );
     });
@@ -120,46 +82,6 @@ describe('_formatRequestUrl api.pricePaid', () => {
 
 describe('_formatRequestUrl api.pricePaid', () => {
     const inputArguments = {
-        buildingName: "",
-        street: "",
-        town: "",
-        postcode: "",
-        detached: "detached",
-        semiDetached: "semi-detached",
-        terraced: "terraced",
-        flat: "flat-maisonette",
-        other: "otherPropertyType",
-        newBuild: "true",
-        notNewBuild: "false",
-        freehold: "freehold",
-        leasehold: "leasehold",
-        minPrice: "",
-        maxPrice: "",
-        dateFrom: "",
-        dateTo: "",
-        perPage: "100",
-    }
-    const argumentMap = {
-        buildingName: "paon=",
-        street: "street=",
-        town: "town=",
-        postcode: "postcode=",
-        detached: "ptype%5B%5D=lrcommon%3A",
-        semiDetached: "ptype%5B%5D=lrcommon%3A",
-        terraced: "ptype%5B%5D=lrcommon%3A",
-        flat: "ptype%5B%5D=lrcommon%3A",
-        other: "ptype%5B%5D=lrcommon%3A",
-        newBuild: "nb%5B%5D=",
-        notNewBuild: "nb%5B%5D=",
-        freehold: "et%5B%5D=lrcommon%3A",
-        leasehold: "et%5B%5D=lrcommon%3A",
-        minPrice: "min_price=",
-        maxPrice: "max_price=",
-        dateFrom: "min_date=",
-        dateTo: "max_date=",
-        perPage: "limit=",
-    }
-    const defaultArguments = {
         buildingName: "",
         street: "",
         town: "",
